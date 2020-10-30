86bb997702edf7915fca59c252c0929896ff040e
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-10-30 03:51:27 +0300

    Client: ConnectionModel

---
+++ Vasilev.SimpleChat.WpfNetCore.Client/Models/ConnectionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace Vasilev.SimpleChat.WpfNetCore.Client.Models
+{
+    internal class ConnectionModel
+    {
+        public TcpClient Tcp { get; internal set; }
+
+        public StreamReader Reader { get; internal set; }
+
+        public StreamWriter Writer { get; internal set; }
+
+    }
+}
