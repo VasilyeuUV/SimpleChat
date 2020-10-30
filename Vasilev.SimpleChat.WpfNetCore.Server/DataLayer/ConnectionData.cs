65299320630b35f619bdd5f9c4b01b05cc860675
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-10-30 04:04:58 +0300

    server: ConnectionData - ConnectedClients

---
+++ Vasilev.SimpleChat.WpfNetCore.Server/DataLayer/ConnectionData.cs
@@ -0,0 +1,24 @@
+﻿using ChatServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vasilev.SimpleChat.ConsNetCore.Server.DataLayer
+{
+    public class ConnectionData
+    {
+
+
+        #region ConnectedClients
+
+        private List<UserModel> _connectedClients = null;
+
+        /// <summary>
+        /// Connected Clients
+        /// </summary>
+        public List<UserModel> ConnectedClients => _connectedClients ??= new List<UserModel>(); 
+
+        #endregion
+
+    }
+}
---
+++ Vasilev.SimpleChat.WpfNetCore.Server/Models/ConnectionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Vasilev.SimpleChat.ConsNetCore.Server.Models
+{
+    public class ConnectionModel
+    {
+        public TcpListener TcpListener { get; set; }
+
+
+
+    }
+}
